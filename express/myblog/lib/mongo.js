00c0b3c7ce600c02661042f982ccaeddd8decf91
volcano <yuanvolcano@163.com>
2018-12-03 23:32:26 +0800

    完成登录界面

--- express/myblog/lib/mongo.js
+++ express/myblog/lib/mongo.js
@@ -1,6 +1,24 @@
 const config = require('config-lite')(__dirname);
 const Mongalass = require('mongolass');
 const mongolass = new Mongalass();
+const moment = require('moment');
+const objectIdToTimestamp = require('objectid-to-timestamp');
+
+
+mongolass.plugin('addCreatedAt', {
+  afterFind: function (results) {
+    results.forEach(function (item) {
+      item.created_at = moment(objectIdToTimestamp(item._id)).format('YYYY-MM-DD HH:mm')
+    })
+    return results
+  },
+  afterFindOne: function (result) {
+    if (result) {
+      result.created_at = moment(objectIdToTimestamp(result._id)).format('YYYY-MM-DD HH:mm')
+    }
+    return result
+  }
+})
 
 mongolass.connect(config.mongodb);
 
--- express/myblog/middlewares/check.js
+++ express/myblog/middlewares/check.js
@@ -1,6 +1,5 @@
 module.exports = {
   checkLogin (req, res, next) {
-    console.log(req.session);
     if (!req.session.user) {
       req.flash('error', '未登录');
       return res.redirect('/signin');
