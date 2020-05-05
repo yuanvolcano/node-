d8808078a85de7dad05ee4eca0a808b95034ba31
袁灿 <can_yuan@kingdee.com>
2020-05-05 21:50:23 +0800

    feature:连接池设置

--- koa/sequelize-test/src/seq.js
+++ koa/sequelize-test/src/seq.js
@@ -5,6 +5,13 @@ const conf = {
   dialect: 'mysql'
 }
 
+// 线上环境，使用连接池
+conf.pool = {
+  max: 5, // 连接池中最大的连接数量
+  min: 0, // 最小
+  idle: 10000 // 如果一个连接池 10s 之内没有被使用，则释放
+}
+
 const seq = new Sequelize('sinaBlog', 'root', 'yuan930905can', conf)
 
 module.exports = seq
