f8583c35d7be9c7e651230d80730c9470b8eb7f6
袁灿 <can_yuan@kingdee.com>
2020-05-27 20:19:16 +0800

    feature: 微博用户模型

---
+++ koa/sina-blob/src/db/models/Blog.js
@@ -0,0 +1,26 @@
+/**
+ * @description User 数据模型
+ * @author volcano
+ */
+
+const seq = require('../seq')
+const { INTEGER, STRING, TEXT } = require('../types')
+
+const Blog = seq.define('blog', {
+  userId: {
+    type: INTEGER,
+    allowNull: false,
+    comment: '用户 ID'
+  },
+  content: {
+    type: TEXT,
+    allowNull: false,
+    comment: '用户内容'
+  },
+  image: {
+    type: STRING,
+    comment: '图片地址'
+  }
+})
+
+module.exports = Blog
